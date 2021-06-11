882e0fc3d89143071cbd60b49a87e38450bdd2c2
Jacquesval <jacquesval48@gmail.com>
2021-06-11 15:28:02 +0200

    add camear info to image_publisher

--- catkin_ws/src/image_publisher/src/image_publisher.cpp
+++ catkin_ws/src/image_publisher/src/image_publisher.cpp
@@ -1,5 +1,6 @@
 #include <ros/ros.h>
 #include <image_transport/image_transport.h>
+#include <camera_info_manager/camera_info_manager.h>
 #include <opencv2/highgui/highgui.hpp>
 #include <cv_bridge/cv_bridge.h>
 #include <std_msgs/Header.h>
@@ -8,10 +9,13 @@ int main(int argc, char** argv)
 {
     ros::init(argc, argv, "image_publisher");
     ros::NodeHandle nh;
+    image_transport::CameraPublisher imagePub;
     image_transport::ImageTransport it(nh);
-    image_transport::Publisher pub = it.advertise("camera/image", 0);
+    imagePub = it.advertiseCamera("camera/image_raw", 1);
+    boost::shared_ptr<camera_info_manager::CameraInfoManager> cameraInfoManager;
 
 
+    cameraInfoManager.reset(new camera_info_manager::CameraInfoManager(nh, "camera", "package://image_publisher/calibration/camera_calibration.yaml"));
     cv::VideoCapture cap(0);
     // Check if video device can be opened with the given index
     if(!cap.isOpened()){
@@ -19,17 +23,20 @@ int main(int argc, char** argv)
         return 1;   
     }
     cv::Mat frame;
-    sensor_msgs::ImagePtr msg;
+    sensor_msgs::ImagePtr image;
+    sensor_msgs::CameraInfoPtr cameraInfo(new sensor_msgs::CameraInfo(cameraInfoManager->getCameraInfo()));
     std_msgs::Header header;
-    ros::Rate loop_rate(15);
+    ros::Rate loop_rate(20);
     header.frame_id = "camera";
+    cameraInfo->header.frame_id = header.frame_id;
     while (nh.ok()) {
         cap >> frame;
         // Check if grabbed frame is actually full with some content
         if(!frame.empty()) {
             header.stamp = ros::Time::now();
-            msg = cv_bridge::CvImage(header, "bgr8", frame).toImageMsg();
-            pub.publish(msg);
+            image = cv_bridge::CvImage(header, "bgr8", frame).toImageMsg();
+            cameraInfo->header.stamp = header.stamp;
+            imagePub.publish(*image, *cameraInfo);
             // cv::waitKey(1);
         }
         else {
